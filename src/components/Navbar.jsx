ea2af9721e90b1830b234df9c6ff46c7db061ee6
Ibrahim <ibz_374@hotmail.com>
2025-05-12 16:38:06 +0100

    Completed changes to Scss files for clear mobile responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,58 +4,48 @@ import '../styles/Navbar.css'
 import ReorderIcon from '@mui/icons-material/Reorder';
 
 const Navbar = () => {
-const [expandNavBar, setExpandNavBar] = useState(false)
+// const [expandNavBar, setExpandNavBar] = useState(false)
 
 
   return (
-    <div className='navbar' id={expandNavBar ? 'open' : 'close'}>
+    <div className='navbar'>
       <div className='navbar-left'>
         <span className='brand'>Ibrahim.dev</span>
-      </div>
-      <div className='toggleButton'>
-        <button onClick={()=> setExpandNavBar((prev) => !prev)}>
-        <ReorderIcon style={{fontSize:50}}/> 
-        </button>
       </div>
       <div className='links'>
         <ScrollLink 
           to="home" 
           smooth={true} 
           duration={500} 
-          className="item"
-          onClick={() => setExpandNavBar(false)}>
+          className="item">
           Home
         </ScrollLink>
         <ScrollLink 
           to="skills" 
           smooth={true} 
           duration={500} 
-          className="item"
-          onClick={() => setExpandNavBar(false)}>
+          className="item">
           Tech Skills
         </ScrollLink>
         <ScrollLink 
           to="projects" 
           smooth={true} 
           duration={500} 
-          className="item"
-          onClick={() => setExpandNavBar(false)}>
+          className="item">
           Projects
         </ScrollLink>
         <ScrollLink 
           to="experience" 
           smooth={true} 
           duration={500} 
-          className="item"
-          onClick={() => setExpandNavBar(false)}>
+          className="item">
           Experience
         </ScrollLink>
         <ScrollLink 
           to="contact" 
           smooth={true} 
           duration={500} 
-          className="item"
-          onClick={() => setExpandNavBar(false)}>
+          className="item">
           Contact me
         </ScrollLink>
       </div>
